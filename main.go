2676804fde090b0c5b22c26dcccbab871d967572
downingmoon <joy97q12@naver.com>
2019-08-26 12:53:28 +0900

    Todo 완료

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main // import "zzz"
 
 import (
-	"net/http"
-	"fmt"
 	_"encoding/json"
+	_"fmt"
+	"log"
+	"net/http"
+	"util"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	_ "github.com/go-sql-driver/mysql"
-	"util"
-	"log"
 )
 
 func main() {
-	fmt.Print("start")
 	// echo 초기화 & CORS 설정
 	e := echo.New()
 	e.Use(middleware.CORS())
@@ -22,34 +21,75 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Home")
 	})
-	// e.GET("/user/:id", getUser)
 
 	// TODO List
 	e.GET("/todos", selectTodoList)
 
 	// TODO Complete
-	// e.PUT("/todos/:idx", todoComplete)
+	e.PUT("/todos/:idx", todoComplete)
 
 	// TODO Delete
-	// e.DELETE("/todos/:idx", deleteTodo)
+	e.DELETE("/todos/:idx", deleteTodo)
 
 	// TODO Insert
-	// e.POST("/todos", insertTodo)
+	e.POST("/todos", insertTodo)
 
 	// 시작
 	e.Logger.Fatal(e.Start(":4000"))
 }
 
 func selectTodoList(c echo.Context) error {
-	rows := util.SelectTodoList()
+	db := util.DBConnection()
+	defer db.Close()
+
+	rows, err := db.Query("SELECT IDX, TODO, ISDONE FROM TODOLIST ORDER BY IDX ASC")
+	if err != nil { log.Fatal(err) }
 	result := util.Todos{}
 	defer rows.Close()
 
-	for rows.Next(){
+	for rows.Next() {
 		todo := util.Todo{}
 		err2 := rows.Scan(&todo.Idx, &todo.Todo, &todo.IsDone)
-		if err2 != nil { log.Fatal(err2) }
+		if err2 != nil {
+			log.Fatal(err2)
+		}
 		result.Todos = append(result.Todos, todo)
 	}
 	return c.JSON(http.StatusOK, result)
+}
+
+func todoComplete(c echo.Context) error {
+	idx := c.Param("idx")
+	db := util.DBConnection()
+	defer db.Close()
+	res, err := db.Query("UPDATE TODOLIST SET ISDONE = '1' WHERE IDX = ?", idx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c.JSON(http.StatusOK, res)
+}
+
+func insertTodo(c echo.Context) error {
+	todo := new(util.Todo)
+	if err := c.Bind(todo); err != nil { log.Fatal(err) }
+
+	db := util.DBConnection()
+	defer db.Close()
+
+	res, err := db.Query("INSERT INTO TODOLIST (TODO) VALUES (?)", todo.Todo)
+	if err != nil { log.Fatal(err) }
+	return c.JSON(http.StatusOK, res)
+}
+
+func deleteTodo(c echo.Context) error {
+	todo := new(util.Todo)
+	if err := c.Bind(todo); err != nil {log.Fatal(err)}
+
+	db := util.DBConnection()
+	defer db.Close()
+
+	log.Println(todo.Idx)
+	res, err := db.Query("DELETE FROM TODOLIST WHERE IDX = ?", todo.Idx)
+	if err != nil { log.Fatal(err) }
+	return c.JSON(http.StatusOK, res)
 }
--- util/DatabaseUtil.go
+++ util/DatabaseUtil.go
@@ -3,14 +3,15 @@ package util // import "util"
 import (
 	_"fmt"
 	"database/sql"
+	"github.com/labstack/echo"
 	"log"
 )
 
 // DB 컬럼
 type Todo struct {
-	Idx int ` json:"idx" `
-	Todo string ` json:"todo" `
-	IsDone string ` json:"isDone"`
+	Idx int ` json:"idx" form:"idx" query:"idx" `
+	Todo string ` json:"todo" form:"todo" `
+	IsDone string ` json:"isDone" form:"isDone" `
 }
 
 type Todos struct {
@@ -33,4 +34,19 @@ func SelectTodoList() *sql.Rows {
 	rows, err := db.Query("SELECT IDX, TODO, ISDONE FROM TODOLIST ORDER BY IDX ASC")
 	if err != nil { log.Fatal(err) }
 	return rows
+}
+
+// TODO 완료
+// @param idx string
+func UpdateTodoComplete(c echo.Context, idx string) *sql.Rows {
+	if err := c.Bind(idx); err != nil {
+		log.Fatal(err)
+	}
+	db := DBConnection()
+	defer db.Close()
+	res, err := db.Query("UPDATE TODO SET ISDONE = '1' WHERE IDX = $1", idx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
 }
